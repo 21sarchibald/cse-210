81889c1f01b5d934069ac75384b4484cf6cd279b
21sarchibald <arc21004@byui.edu>
2024-10-31 10:31:46 -0600

    Added code 10/31

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -7,3 +7,34 @@ class Program
         Console.WriteLine("Hello Sandbox World!");
     }
 }
+
+
+
+// class Program
+// {
+
+//     // void tells the computer the function doesn't return anything.
+//     // static means it can run without having an object associated with it.
+//     // public means anyone can use it
+//     public static void Main(string[] args)
+//     {
+//         Console.WriteLine("Input your age: ");
+//         string userInput = Console.ReadLine();
+//         int age = int.Parse(userInput); // Parse turns an integer string into an integer data type.
+
+//         Console.WriteLine($"Your age is: {age}"); // Format output with a $
+
+//         // Console.WriteLine("Hello, World!");
+//         // Console.WriteLine("Bonjour Tout le Monde.")
+
+//         int x = 10; // You must give your variable a type, and you must give your parameters a type in functions.
+//         if (x > 15)
+//         {
+//             Console.WriteLine("Hey Bob");
+//         }
+//         // Console.WriteLine("Please enter your name");
+//         // string name = Console.ReadLine();
+
+//         // Console.WriteLine("$Your name is: {name}\n");
+//     }
+// }
