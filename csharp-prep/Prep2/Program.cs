d000780f3de6dd7bde6fb9bfeda18711c32b3ff3
21sarchibald <arc21004@byui.edu>
2024-11-01 15:29:33 -0600

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradeInput = Console.ReadLine();
+        float grade = float.Parse(gradeInput);
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Letter Grade: {letter}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else 
+        {
+            Console.WriteLine("Sorry, but you failed. Good luck next time!");
+        }
     }
 }
