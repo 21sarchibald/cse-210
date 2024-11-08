13ffc9fdc7017ecc8702c41a2636af0dcd30fcdd
21sarchibald <arc21004@byui.edu>
2024-11-08 10:24:35 -0700

    Finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,17 +1,43 @@
 using System;
 using System.Globalization;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
-        int number;
+        List <double> numberList = new List<double>();
+        double number;
+        double total = 0;
+        double highestValue = 0;
+
         do
         {
             Console.Write("Enter number: ");
-            number = int.Parse(Console.ReadLine());
+            number = double.Parse(Console.ReadLine());
+            if (number != 0) 
+            {
+                numberList.Add(number);
+            }
         }
         while (number != 0);
+
+        foreach (double value in numberList)
+        {
+            if (value > highestValue)
+            {
+                highestValue = value;
+            }
+            total += value;
+        
+        }
+        double average = total/numberList.Count;
+
+        Console.WriteLine($"The sum is: {total}");
+
+        Console.WriteLine($"The average is: {average}");
+
+        Console.WriteLine($"The largest number is: {highestValue}");
     }
 }
