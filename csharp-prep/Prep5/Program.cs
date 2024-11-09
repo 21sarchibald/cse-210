09e50e075e67fa75820de29f1837b871754f1ac8
21sarchibald <arc21004@byui.edu>
2024-11-08 22:34:10 -0700

    Finish prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -9,34 +9,28 @@ class Program
         DisplayResult();
 
     }
-
-
     static void DisplayWelcome()
     {
         Console.WriteLine("Welcome to the Program!");
     }
-
     static string PromptUserName()
     {
         Console.Write("Please enter your name: ");
         string nameInput = Console.ReadLine();
         return nameInput;
     }
-
     static int PromptUserNumber()
     {
         Console.Write("Please enter your favorite number: ");
         int numberInput = int.Parse(Console.ReadLine());
         return numberInput;
     }
-
     static int SquareNumber()
     {
         int number = PromptUserNumber();
         int numberSquared = number * number;
         return numberSquared;
     }
-
     static void DisplayResult()
     {
         string name = PromptUserName();
