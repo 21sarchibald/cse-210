3ad22947d91d97f2e39bc93bf4b1f22cbb242e2b
21sarchibald <arc21004@byui.edu>
2024-11-08 22:33:38 -0700

    Finished prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        DisplayResult();
+
+    }
+
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string nameInput = Console.ReadLine();
+        return nameInput;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int numberInput = int.Parse(Console.ReadLine());
+        return numberInput;
+    }
+
+    static int SquareNumber()
+    {
+        int number = PromptUserNumber();
+        int numberSquared = number * number;
+        return numberSquared;
+    }
+
+    static void DisplayResult()
+    {
+        string name = PromptUserName();
+        int numberSquared = SquareNumber();
+        Console.WriteLine($"{name}, the square of your number is {numberSquared}");
     }
 }
