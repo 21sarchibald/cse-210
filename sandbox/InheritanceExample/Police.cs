00c3c9313feab141c5e8b9317be426b10d7fc800
21sarchibald <arc21004@byui.edu>
2024-11-25 10:00:09 -0700

    Finished InheritanceExample

---
+++ sandbox/InheritanceExample/Police.cs
@@ -0,0 +1,13 @@
+class Police : Person
+{
+    private string _weapons;
+    public Police(string firstName, string lastName, int age, string weapons) :base(firstName, lastName, age)
+    {
+        _weapons = weapons;
+    }
+
+    public string GetPoliceInformation()
+    {
+        return $"Weapons: {_weapons}, {GetPersonInfo()} {_firstName}";
+    }
+}
---
+++ sandbox/InheritanceExample/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Inheritance");
+        Person bob = new Person("Bob", "Billy", 37);
+        Person betty = new("Betty", "Billy", 39);
+
+        Console.WriteLine(bob.GetPersonInfo());
+        Console.WriteLine(betty.GetPersonInfo());
+
+        Doctor doctorBob = new Doctor("Bob", "Budge", 65, "Hack Saw");
+        Console.WriteLine(doctorBob.GetDoctorInformation());
+        Console.WriteLine(doctorBob.GetPersonInfo());
+
+        Police policeDoug = new Police("Doug", "Denver", 40, "Taser");
+        Console.WriteLine(policeDoug.GetPoliceInformation());
+        Console.WriteLine(policeDoug.GetPersonInfo());
+    }
+}
