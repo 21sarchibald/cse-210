b6b79ae72de9009170150e1ec5fc7429c902f800
21sarchibald <arc21004@byui.edu>
2024-12-03 22:40:12 -0700

    Finished Rectangle class

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -8,5 +8,9 @@ class Program
         Square square = new Square("red", 3);
         Console.WriteLine(square.GetColor());
         Console.WriteLine(square.GetArea());
+
+        Rectangle rectangle = new Rectangle("yellow", 1, 2);
+        Console.WriteLine(rectangle.GetColor());
+        Console.WriteLine(rectangle.GetArea());
     }
 }
