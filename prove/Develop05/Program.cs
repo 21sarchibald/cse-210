ab8088245d18c433031a4e2e4c2cbd2b2656ec0b
21sarchibald <arc21004@byui.edu>
2024-12-05 10:00:00 -0700

    Added functionality to get the goal type

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -11,13 +11,30 @@ class Program
     {
         do
         {
-            string _menu = "Menu Options:\n    1. Create New Goal\n    2. List Goals\n    3. Save Goals\n    4. Load Goals\n    5. Record Event\n    6. Quit\nSelect a choice from the menu: ";
-            DisplayMenu(_menu);
+            string _mainMenu = "Menu Options:\n    1. Create New Goal\n    2. List Goals\n    3. Save Goals\n    4. Load Goals\n    5. Record Event\n    6. Quit\nSelect a choice from the menu: ";
+            string _goalTypeMenu = "The types of Goals are:\n    1. Simple Goal\n    2. Eternal Goal\n    3. Checklist Goal\nWhich type of goal would you like to create? ";
+            DisplayMenu(_mainMenu);
             _menuSelection = GetMenuSelection();
 
             if (_menuSelection == 1)
             {
                 Console.WriteLine("hello");
+                DisplayMenu(_goalTypeMenu);
+                int _goalType = GetMenuSelection();
+
+                if (_goalType == 1)
+                {
+                    Console.WriteLine("type 1)");
+                }
+                if (_goalType == 2)
+                {
+
+                }
+                if (_goalType == 3)
+                {
+
+                }
+
             }
             else if (_menuSelection == 2)
             {
